f061df55c18f68142a94cebd82b2c8e4ed0416b0
GZH <guozh29@mail2.sysu.edu.cn>
2025-04-19 03:00:10 +0800

    Update cross_all.bat

--- example/cross_all.bat
+++ example/cross_all.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python knowledge_transfer.py --city AMS+JHB+LOA+MEL+SPO+SZH --max_stations 30 --eval_percentage 20 --auxiliary all --model frets --pred_type city --global_epoch 100 --deploy_epoch 20
+python knowledge_transfer.py --city AMS+JHB+LOA+MEL+SPO+SZH --max_stations 30 --eval_percentage 20 --auxiliary all --model frets --pred_type station --global_epoch 100 --deploy_epoch 20 --output_path ./result/federated/all/
 
 echo All experiments completed.
